847efac3de6aba41354c7c142e0313600736b82d
Admin <Admin@DESKTOP-BROSEOT.COM>
2020-07-30 14:40:23 +0800

    我的界面

---
+++ app/src/main/java/com/cbsd/investment/view/ResizableImageView.java
@@ -0,0 +1,30 @@
+package com.cbsd.investment.view;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+public class ResizableImageView extends AppCompatImageView {
+
+    public ResizableImageView(Context context) {
+        super(context);
+    }
+
+    public ResizableImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Drawable d = getDrawable();
+        if (d != null) {
+            int width = MeasureSpec.getSize(widthMeasureSpec);
+            int height = (int) Math.ceil((float) width * (float) d.getIntrinsicHeight() / (float) d.getIntrinsicWidth());
+            setMeasuredDimension(width, height);
+        } else {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        }
+    }
+}
